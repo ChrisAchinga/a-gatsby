dec9a17c5d7031a4449f4bf0a442c094913cc2eb
ChrisAchinga <achinga.chris@gmail.com>
2020-08-27 23:14:32 +0300

    props

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Header from "../components/header" // imports header as a component
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header />
+      <Header headerText='About Page'/>
       <p>Such wow. Very React.</p>
     </div>
   )
