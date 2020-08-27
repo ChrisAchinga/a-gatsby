3ae10d2b1c98707a1e936d8bd8132575b3090709
ChrisAchinga <achinga.chris@gmail.com>
2020-08-27 23:35:27 +0300

    add link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby";
+import Header from "../components/header";
 
 export default function Home() {
   return <div style={{ color: `blue`}}>
-  <h1>Gatsby JS</h1>
+  <Link to="/contact/">Contact</Link>
+  <Header headerText='Home Page'/>
   <p>A react framework</p>
   <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
