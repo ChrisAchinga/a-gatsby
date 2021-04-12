51416ddcb60de50a950e54249364a7254c49c399
ChrisAchinga <achinga.chris@gmail.com>
2021-04-12 12:23:36 +0300

    btn

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ActivityIndicator, Button } from 'react-native'
 
 const App = () => {
+  const onBtnPress = () => {
+    console.log(`${new Date().toLocaleTimeString()} button press`)
+  }
   return (
     <View style={{ padding: 100 }}>
+      <ActivityIndicator siz='large' color='#61DBFB' />
       <Text>Hello World</Text>
+      <Button title='Withdraw Ideas' onPress={onBtnPress} />
     </View>
   )
 }
